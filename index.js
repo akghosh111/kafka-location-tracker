62ef048b26a5b208c7b1436266b09005a7e9dc6f
akghosh111 <ghoshkiran2017@gmail.com>
2026-05-03 10:26:44 +0530

    add kafka consumer & client

--- index.js
+++ index.js
@@ -4,20 +4,44 @@ import path from "node:path";
 import express from "express";
 import {Server} from "socket.io";
 
+import { kafkaClient } from "./kafka-client.js"
+
 async function main() {
     const PORT = process.env.PORT ?? 8000;
     const app = express();
     const server = http.createServer(app);
     const io = new Server();
 
+    const kafkaProducer = kafkaClient.producer();
+    await kafkaProducer.connect();
+
+    const kafkaConsumer = kafkaClient.consumer({ groupId: `socket-server-${PORT}` });
+    await kafkaConsumer.connect();
+
+    await kafkaConsumer.subscribe({ topics: ['location-updates'], fromBeginning: true });
+
+    kafkaConsumer.run({
+        eachMessage: async({topic, partition, message, heartbeat}) => {
+            const data = JSON.parse(message.value.toString());
+            console.log(`Kafka Consumer Data Received`, { data });
+            await heartbeat();
+        }
+    })
+
     io.attach(server);
 
     io.on("connection", (socket) => {
         console.log(`[Socket:${socket.id}]: Connected successfully`);
 
-        socket.on("client:location:update", (locationData) => {
+        socket.on("client:location:update", async (locationData) => {
             const { latitude, longitude } = locationData;
             console.log(`[Socket:${socket.id}]: client:location:update`, locationData);
+
+            await kafkaProducer.send({ topic: "location-updates", messages:  [{
+                key: socket.id,
+                value: JSON.stringify({ id: socket.id, latitude, longitude })
+            }] })
+
         }) 
     })
 
